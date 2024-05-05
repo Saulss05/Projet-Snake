ae0ce4dc48663f0fc189a57c192686996dfdbcae
Saulss05 <saulduqueyroix@gmail.com>
2024-05-05 23:53:16 +0200

    Update Liste_Section.h

--- projet_snake/Liste_Section.h
+++ projet_snake/Liste_Section.h
@@ -1,8 +1,34 @@
 #ifndef _SERPENT_H
 #define _SERPENT_H
 
-struct liste_chaine{
-    
+struct section{
+    int taille;
+    char* couleur; 
+    struct section*suivant;
+};
+
+struct liste{
+    struct section *premier;
+    struct section *dernier;
+    int longueur;
+};
+
+struct section *creer_section (int,char*);
+
+void desallouer_section (struct section **);
+
+struct liste *nouvelle_liste();
+
+void ajouter_section_debut(struct liste *, struct section *);
+
+void ajouter_section_queue(struct liste *, struct section *);
+
+struct section *extraire_maillon_debut(struct liste *);
+
+void detruire_liste (struct liste **);
+
+
+#endif
 };
 	
 #endif
