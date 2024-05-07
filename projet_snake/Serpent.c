5e81c8d088bf184a964691c5ffe7b7751bda93a5
Saulss05 <saulduqueyroix@gmail.com>
2024-05-07 16:25:36 +0200

    Update Serpent.c

--- projet_snake/Serpent.c
+++ projet_snake/Serpent.c
@@ -1,3 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
 #include"Serpent.h"
 #include"Grille.h"
-#include"Liste_Section.h"
+		
+		
+void Grille_remplir_serpent(struct grille * G,struct serpent * S){
+	S->tete.x = G->n/2;
+	S->tete.y = G->m/2;
+	G->tab[S->tete.x][S->tete.y] = "\33[42m:p";
+}
+/*	int i;
+	struct section*N=S->corps->premier;
+	struct coord dernier;
+*/	
+	
+	
