965a809682998cdfe41251a1e9d82f182d858d41
Saulss05 <saulduqueyroix@gmail.com>
2024-05-07 16:25:21 +0200

    Update Liste_Section.c

--- projet_snake/Liste_Section.c
+++ projet_snake/Liste_Section.c
@@ -6,14 +6,13 @@
 struct section *creer_section (int t,char*c) {
   struct section *s = malloc(sizeof(struct section));
   s->taille = t;
-  s->couleur=c;
+  s->couleur = c;
   s->suivant = NULL;
   return s;
 }
 
 void desallouer_section (struct section **m) {
   if (*m != NULL) {
-    free((*m)->taille);
     free((*m)->couleur);
     free(*m);
     *m = NULL;
@@ -37,7 +36,7 @@ void ajouter_section_debut(struct liste *l, struct section *s) {
   ++l->longueur;
 }
 
-void ajouter_maillon_queue(struct liste *l, struct section*s) {
+void ajouter_section_queue(struct liste *l, struct section*s) {
   s->suivant = NULL;
   if (l->longueur==0) {
     l->premier = s;
@@ -49,7 +48,7 @@ void ajouter_maillon_queue(struct liste *l, struct section*s) {
   ++l->longueur;
 }
 
-struct section *extraire_maillon_debut(struct liste *l) {
+struct section *extraire_section_debut(struct liste *l) {
   struct section *s = l->premier;
   if (s != NULL) {
     l->premier = s->suivant;
@@ -65,7 +64,7 @@ struct section *extraire_maillon_debut(struct liste *l) {
 void detruire_liste (struct liste **l){
   if (*l != NULL) {
     while ((*l)->longueur!=0) {
-      struct section *s = extraire_maillon_debut_liste(*l);
+      struct section *s = extraire_section_debut(*l);
       desallouer_section(&s);
     }
     free(*l);
