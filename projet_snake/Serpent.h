39c402d6ab0979e4433435df40ba3e0d7f45783d
Saulss05 <saulduqueyroix@gmail.com>
2024-05-13 01:24:54 +0200

    Update Serpent.h

--- projet_snake/Serpent.h
+++ projet_snake/Serpent.h
@@ -1,13 +1,21 @@
 #ifndef _SERPENT_H
 #define _SERPENT_H
+#include"Liste_Section.h"
 #include"Grille.h"
+#include"Liste_Mouvement.h"
 
 struct serpent{
 	struct coord tete;
 	struct liste *corps;
+	struct liste_mvt *mvt;
 };
 
+
+
 struct serpent * creer_serpent(int x,int y);
+	
+//char*generer_couleur();
 
 void Grille_remplir_serpent(struct grille*G ,struct serpent*S);
+
 #endif
