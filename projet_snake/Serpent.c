ac2519e84e8977a9de4699167e8e6e3f90d9a483
Saulss05 <saulduqueyroix@gmail.com>
2024-05-13 01:24:14 +0200

    Update Serpent.c

--- projet_snake/Serpent.c
+++ projet_snake/Serpent.c
@@ -1,5 +1,6 @@
 #include"Serpent.h"
 #include<stdlib.h>
+#include<string.h>
 #include<stdio.h>
 #include"Grille.h"
 		
@@ -8,23 +9,84 @@ struct serpent * creer_serpent(int x,int y){
 	S->tete.x=x;
 	S->tete.y=y;
 	S->corps = nouvelle_liste();
+	S->mvt=nouvelle_liste_mvt();
 	return S;
 }		
-		
+
+/*char*generer_couleur(){
+	char*couleur=malloc(sizeof(char)*8);
+	return couleur;
+}*/
+
+
+
+
 void Grille_remplir_serpent(struct grille * G,struct serpent * S){
 	int i;
 	struct section *M=S->corps->premier;
+	struct section_mvt *Sec=S->mvt->premier;
 	struct coord dernier;
 	G->tab[S->tete.x][S->tete.y] = "\33[42m:p";
 	dernier.x=S->tete.x;
 	dernier.y=S->tete.y;
+	//M->couleur=generer_couleur();
 	while(M!=NULL){
-		for(i=0;i<M->taille;i++){
-			dernier.x+=1;
-			G->tab[dernier.y][dernier.x]=M->couleur;
+		switch(Sec->direction){
+		
+			case 1:
+				for(i=0;i<M->taille;i++){
+					dernier.x++;
+					G->tab[dernier.x][dernier.y]=M->couleur;
+					if (dernier.x==Sec->coord.x && dernier.y==Sec->coord.y){
+						Sec=Sec->suivant;
+						break;
+					}
+				}
+				break;
+				
+			case 2:
+				for(i=0;i<M->taille;i++){
+					dernier.y--;
+					G->tab[dernier.x][dernier.y]=M->couleur;
+					if (dernier.x==Sec->coord.x && dernier.y==Sec->coord.y){
+						Sec=Sec->suivant;
+						break;
+					}
+				}
+				break;
+			
+			case 3:
+				for(i=0;i<M->taille;i++){
+					dernier.x--;
+					G->tab[dernier.x][dernier.y]=M->couleur;
+					if (dernier.x==Sec->coord.x && dernier.y==Sec->coord.y){
+						Sec=Sec->suivant;
+						break;
+					}
+				}
+				break;
+			
+			case 4:
+				for(i=0;i<M->taille;i++){
+					dernier.y++;
+					G->tab[dernier.x][dernier.y]=M->couleur;
+					if (dernier.x==Sec->coord.x && dernier.y==Sec->coord.y){
+						Sec=Sec->suivant;
+						break;
+					}
+				}
+				break;
+	
+			default:
+				break;
 		}
+	
+	
 		M=M->suivant;
 	}
 }
+	
+	
+
 	
 	
