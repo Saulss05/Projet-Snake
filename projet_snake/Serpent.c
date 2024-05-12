5118995e02258deb2714e650f85ec6528a5e3093
Saulss05 <saulduqueyroix@gmail.com>
2024-05-12 18:56:16 +0200

    Update Serpent.c

--- projet_snake/Serpent.c
+++ projet_snake/Serpent.c
@@ -1,40 +1,30 @@
+#include"Serpent.h"
 #include<stdlib.h>
 #include<stdio.h>
-#include"Serpent.h"
 #include"Grille.h"
-#include"Liste_Section.h"
 		
-struct serpent * creer_serpent(int x , int y){
+struct serpent * creer_serpent(int x,int y){
 	struct serpent * S = malloc(sizeof(struct serpent));
 	S->tete.x=x;
 	S->tete.y=y;
 	S->corps = nouvelle_liste();
 	return S;
-
 }		
 		
 void Grille_remplir_serpent(struct grille * G,struct serpent * S){
-
 	int i;
 	struct section *M=S->corps->premier;
 	struct coord dernier;
-	
-	
-	G->tab[S->tete.x][S->tete.y] = "\33[42m  ";
-	
+	G->tab[S->tete.x][S->tete.y] = "\33[42m:p";
 	dernier.x=S->tete.x;
 	dernier.y=S->tete.y;
 	while(M!=NULL){
 		for(i=0;i<M->taille;i++){
 			dernier.x+=1;
-			G->tab[dernier.x][dernier.y]="\33[42  ";
+			G->tab[dernier.y][dernier.x]=M->couleur;
 		}
 		M=M->suivant;
 	}
 }
-/*	int i;
-	struct section*N=S->corps->premier;
-	struct coord dernier;
-*/	
 	
 	
