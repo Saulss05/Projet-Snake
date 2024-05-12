73112509d52946d7e133ab83ac9757b35032275f
Saulss05 <saulduqueyroix@gmail.com>
2024-05-13 01:20:58 +0200

    Update Grille.c

--- projet_snake/Grille.c
+++ projet_snake/Grille.c
@@ -10,7 +10,7 @@
 struct grille *Grille_allouer(int n,int m){
 	int i;
 	int j;
-	struct grille* G=(struct grille*)malloc(sizeof(struct grille));
+	struct grille* G=malloc(sizeof(struct grille));
 	G->n=n;
 	G->m=m;
 	if(G==NULL){
@@ -20,7 +20,7 @@ struct grille *Grille_allouer(int n,int m){
 	G->tab=malloc(sizeof(char**)*(G->n));
 	for(i=0;i<G->n;i++){
 		*(G->tab+i)=malloc(sizeof(char*)*(G->m));
-		for(j=0;G->m;j++){
+		for(j=0;j<G->m;j++){
 			*(*(G->tab+i)+j)=malloc(sizeof(char) * 8);
 		}
 	}
@@ -28,88 +28,53 @@ struct grille *Grille_allouer(int n,int m){
 }
 
 void Grille_vider(struct grille*G){
-    int i,j;
-    for(i=0;i<G->n;i++){
-        for (j=0;j<G->m;j++){
-            G->tab[i][j]="\33[00m  ";
-        }
-
+	int i;
+	int j;
+	for(i=0;i<G->n;i++){
+		for(j=0;j<G->m;j++){
+			G->tab[i][j]="\33[40m  ";
+		}
 	}
 }
 
 void Grille_tirage_fruit(struct grille*G){
-	(G->fruit).x=rand()%(G->n); // pas n-1? 
-        (G->fruit).y=rand()%(G->m);// si x et y == 0 ajouter fonction while 
+	G->fruit.x=rand()%(G->n);
+	G->fruit.y=rand()%(G->m);
 }
 
 void Grille_remplir(struct grille*G){
-	 G->tab[G->fruit.x][G->fruit.y]="\33[41m  ";
+	G->tab[G->fruit.x][G->fruit.y]="\33[41m  ";
 }
 
 void Grille_desallouer(struct grille*G){
-	int i,j;
-    for ( i = 0; i < G->n; i++){
-        for (j = 0; j < G->m; j++){
-            free(G->tab[i][j]);
-        }
-        free(G->tab[i]);
-    }
-    free(G);
+	int i;
+	int j;
+	for(i=0;i<G->n;i++){
+		for(j=0;j<G->m;j++){
+			free(G->tab[i][j]);
+		}
+		free(G->tab[i]);
+	}
+	free(G);
 }
-	
-void Grille_redessiner(struct grille*G){
-	int i,j,k;
-    for(k=0;k<G->m+2; k++)
-    printf("\33[44m  ");
-    printf("\33[00m");
-    printf("\n");
-    for(i=0;i<G->n; i++)
-    {
-        printf("\33[44m  ");
-        for(j=0;j<G->m;j++)
-        {
-            printf("%s",G->tab[i][j]);
 
-        }
-        printf("\33[44m  ");
-        printf("\33[00m  ");
-        printf("\n");
-    }
+void Grille_redessiner(struct grille*G){
+    int i,j,k;
     for(k=0;k<G->m+2; k++)
-    printf("\33[44m  ");
+    	printf("\33[44m  ");
+    printf("\33[00m  ");
+    printf("\n\r");
+        for(i=0;i<G->n; i++){
+            printf("\33[44m  ");
+            for(j=0;j<G->m;j++){
+                printf("%s",G->tab[i][j]);
+            }
+            printf("\33[44m  ");
+            printf("\33[00m  ");
+            printf("\n\r");
+        }
+    for(k=0;k<G->m+2; k++)
+            printf("\33[44m  ");
     printf("\33[00m  ");
     printf("\n");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	
