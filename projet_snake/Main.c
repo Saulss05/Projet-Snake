fd1a6485251c416cac62c7c9edfe3dfdc1c65d15
Saulss05 <saulduqueyroix@gmail.com>
2024-05-12 18:54:18 +0200

    Update Main.c

--- projet_snake/Main.c
+++ projet_snake/Main.c
@@ -10,25 +10,31 @@ int main(int argc,char**argv){
 	srand(time(NULL));
 	int n;
 	int m;
+	int taille=4;
+	char*couleur="\33[42m  ";
 	int temp;
-	//int jeux;
-	if(argc!=4){
+	int jeux;
+	if(argc!=5){
 		fprintf(stderr,"Erreur nombre d'arguments\n");
 		return EXIT_FAILURE;
 	}
-	
+	jeux=atoi(argv[4]);
+	if(jeux!=1){
+		printf("Erreur nom du jeu\n");
+		}		
 	temp=atoi(argv[3]);
 	halfdelay(temp);
-	  
 	n=atoi(argv[1]);
 	m=atoi(argv[2]);
 	struct grille *G=Grille_allouer(n,m);
-	struct serpent *S=malloc(sizeof(struct serpent));
 	Grille_vider(G);
 	Grille_tirage_fruit(G);
 	Grille_remplir(G);
-	Grille_remplir_serpent(G,S);
-	jeu(G,S);
+	struct serpent *Snake=creer_serpent((G->n)/2,(G->m)/2);
+	struct section *Sec=creer_section(taille,couleur);
+	ajouter_section_debut(Snake->corps,Sec);
+	Grille_remplir_serpent(G,Snake);
+	jeu(G,Snake);
 	Grille_redessiner(G);
 }	
 	
