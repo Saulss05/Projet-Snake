60c179919204d3dfc1377d1130ca824905a19942
Saulss05 <saulduqueyroix@gmail.com>
2024-05-12 18:53:00 +0200

    Update Fonction_Jeu_1_serpent.h

--- projet_snake/Fonction_Jeu_1_serpent.h
+++ projet_snake/Fonction_Jeu_1_serpent.h
@@ -3,10 +3,11 @@
 #include"Grille.h"
 #include "Serpent.h"
 
-
-
 void jeu(struct grille* G, struct serpent* S);
+
 int gameover1(struct grille* G, struct serpent* S);
+
+void manger_fruit(struct serpent* S);
 
 #endif
 
